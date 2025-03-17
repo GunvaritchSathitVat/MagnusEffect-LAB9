d4b57afac0568efec0cd4342fbb3554306397049
BuBuu1999 <buranapohrn.jaiy@bumail.net>
2025-03-17 19:22:15 +0700

    Rotate Update B

--- Assets/Scripts/Rottate A.cs
+++ Assets/Scripts/Rottate A.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class RottateA : MonoBehaviour
 {
     Rigidbody rb;
-   [SerializeField] Vector3 angularV;
+   [SerializeField] Vector3 angularV, torque;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,9 +15,17 @@ public class RottateA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
           rb.angularVelocity  = angularV;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            rb.AddTorque(torque);
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            rb.angularVelocity = Vector3.zero;            
         }
     }
 }
