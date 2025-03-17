a69175a3c0ac20abf816db36ed46b465589f509f
BuBuu1999 <buranapohrn.jaiy@bumail.net>
2025-03-17 19:42:15 +0700

    Add MagnusEffect, Force

--- Assets/Scripts/Magnus Effect.cs
+++ Assets/Scripts/Magnus Effect.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class MagnusEffect : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] Vector3 velocity, spin;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Kick();
+
+            ApplyMagnusEffect();
+        }
+    }
+
+    void Kick()
+    {
+        rb.velocity = velocity;
+        rb.angularVelocity = spin;
+    }
+
+    void ApplyMagnusEffect()
+    {
+        Vector3 magnusForce = Vector3.Cross(rb.linearVelocity, rb.angularVelocity);
+
+        rb.AddForce(magnusForce, ForceMode.Force);
     }
 }
